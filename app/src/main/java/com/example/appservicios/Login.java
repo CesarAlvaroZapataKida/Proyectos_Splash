902d8eedbb6cc9633aeb1d5a487f45a5ff92f261
CesarAlvaroZapataKida <54584224+CesarAlvaroZapataKida@users.noreply.github.com>
2021-05-17 17:01:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appservicios/Login.java
@@ -0,0 +1,18 @@
+package com.example.appservicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
